75b2c1864505103e9572413df9c96f43c5319a83
Nguyenhuy2801 <nguyenhuy2899@gmail.com>
2024-05-28 02:27:26 +0700

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="home">
+    <contact-form></contact-form>
+  </div>
+</template>
+
+<script>
+import ContactForm from '@/components/ContactForm.vue'
+
+export default {
+  name: "HomeView",
+  components: {
+    ContactForm
+  }
+}
+</script>
